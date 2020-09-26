QwadwoNyamekye/purplship

draft fedex pickup service integration

extensions/canadapost/purplship/carriers/canadapost/pickup/cancel_pickup.py
@@ -1,0 +1,20 @@
+from typing import Tuple, List
+from purplship.core.models import PickupCancellationRequest, Message, ConfirmationDetails
+from purplship.core.utils import Serializable, Element
+from purplship.carriers.canadapost.error import parse_error_response
+from purplship.carriers.canadapost.utils import Settings
+
+
+def parse_cancel_pickup_response(response: Element, settings: Settings) -> Tuple[ConfirmationDetails, List[Message]]:
+    errors = parse_error_response(response, settings)
+    cancellation = ConfirmationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+        success=True
+    ) if len(errors) == 0 else None
+
+    return cancellation, errors
+
+
+def cancel_pickup_request(payload: PickupCancellationRequest, _) -> Serializable[str]:
+    return Serializable(payload.confirmation_number)

extensions/canadapost/purplship/carriers/canadapost/pickup/create_pickup.py
@@ -1,0 +1,33 @@
+from functools import partial
+from pycanadapost.pickup import pickup_availability
+from purplship.core.utils.pipeline import Job, Pipeline
+from purplship.core.utils import to_xml
+from purplship.core.models import PickupRequest
+from purplship.carriers.canadapost.utils import Settings
+from purplship.carriers.canadapost.pickup.request_pickup import pickup_request
+
+
+def create_pickup_request(payload: PickupRequest, settings: Settings):
+    return Pipeline(
+        get_availability=lambda *_: partial(_get_pickup_availability, payload=payload)(),
+        create_pickup=partial(_create_pickup, payload=payload, settings=settings)
+    )
+
+
+def _create_pickup(availability_response: str, payload: PickupRequest, settings: Settings):
+    availability = pickup_availability()
+    node = next(
+        iter(to_xml(availability_response).xpath(".//*[local-name() = $name]", name="pickup-availability")), None
+    )
+    if node is not None:
+        availability.build(node)
+
+    return Job(
+        id="create_pickup",
+        data=pickup_request(payload, settings) if availability.on_demand_tour else None,
+        fallback=""
+    )
+
+
+def _get_pickup_availability(payload: PickupRequest):
+    return Job(id="availability", data=payload.address.postal_code)

extensions/fedex/purplship/carriers/fedex/package/pickup/availability_service.py
@@ -1,0 +1,61 @@
+from pyfedex.pickup_service_v20 import (
+    PickupAvailabilityRequest,
+    TransactionDetail,
+    VersionId,
+    Address,
+    CarrierCodeType,
+    PickupRequestType,
+    AssociatedAccount,
+    AssociatedAccountNumberType
+)
+from purplship.core.models import PickupRequest
+from purplship.core.utils import Serializable, export, create_envelope, apply_namespaceprefix, Envelope, concat_str
+from purplship.carriers.fedex.utils import Settings
+
+
+def pickup_availability_request(payload: PickupRequest, settings: Settings) -> Serializable[PickupAvailabilityRequest]:
+    request_type = next(
+        (PickupRequestType[s] for s in PickupRequestType.__members__ if s.name == payload.service),
+        PickupRequestType.FUTURE_DAY
+    ).value
+
+    request = PickupAvailabilityRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="disp", Major=17, Intermediate=0, Minor=0),
+        PickupType=None,
+        AccountNumber=AssociatedAccount(
+            Type=AssociatedAccountNumberType.FEDEX_EXPRESS,
+            AccountNumber=settings.account_number
+        ),
+        PickupAddress=Address(
+            StreetLines=concat_str(payload.address.address_line1, payload.address.address_line2),
+            City=payload.address.city,
+            StateOrProvinceCode=payload.address.state_code,
+            PostalCode=payload.address.postal_code,
+            CountryCode=payload.address.country_code,
+            Residential=payload.address.residential,
+        ),
+        PickupRequestType=request_type,
+        DispatchDate=payload.date,
+        NumberOfBusinessDays=None,
+        PackageReadyTime=f'{payload.ready_time}:00',
+        CustomerCloseTime=f'{payload.closing_time}:00',
+        Carriers=[CarrierCodeType.FDXE],
+        ShipmentAttributes=None,
+        PackageDetails=None
+    )
+
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: PickupAvailabilityRequest) -> str:
+    envelope: Envelope = create_envelope(body_content=request)
+    envelope.Body.ns_prefix_ = envelope.ns_prefix_
+    apply_namespaceprefix(envelope.Body.anytypeobjs_[0], "v17")
+
+    return export(
+        envelope,
+        namespacedef_='xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v17="http://fedex.com/ws/pickup/v17"'
+    )

extensions/fedex/purplship/carriers/fedex/package/pickup/cancel_service.py
@@ -1,0 +1,56 @@
+from typing import Tuple, List
+from pyfedex.pickup_service_v20 import (
+    CancelPickupRequest,
+    TransactionDetail,
+    VersionId,
+    CarrierCodeType
+)
+from purplship.core.models import PickupCancellationRequest, ConfirmationDetails, Message
+from purplship.core.utils import Serializable, export, create_envelope, apply_namespaceprefix, Envelope, Element
+from purplship.carriers.fedex.error import parse_error_response
+from purplship.carriers.fedex.utils import Settings
+
+
+def parse_cancel_pickup_reply(response: Element, settings: Settings) -> Tuple[ConfirmationDetails, List[Message]]:
+    errors = parse_error_response(response, settings)
+    cancellation = ConfirmationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+        success=True
+    ) if len(errors) == 0 else None
+
+    return cancellation, errors
+
+
+def cancel_pickup_request(payload: PickupCancellationRequest, settings: Settings) -> Serializable[CancelPickupRequest]:
+
+    request = CancelPickupRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="disp", Major=17, Intermediate=0, Minor=0),
+        CarrierCode=[CarrierCodeType.FDXE.value],
+        PickupConfirmationNumber=payload.confirmation_number,
+        ScheduledDate=payload.pickup_date,
+        EndDate=None,
+        Location=None,
+        Remarks=None,
+        ShippingChargesPayment=None,
+        Reason=payload.reason,
+        ContactName=payload.address.person_name,
+        PhoneNumber=payload.address.phone_number,
+        PhoneExtension=None
+    )
+
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: CancelPickupRequest) -> str:
+    envelope: Envelope = create_envelope(body_content=request)
+    envelope.Body.ns_prefix_ = envelope.ns_prefix_
+    apply_namespaceprefix(envelope.Body.anytypeobjs_[0], "v17")
+
+    return export(
+        envelope,
+        namespacedef_='xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v17="http://fedex.com/ws/pickup/v17"'
+    )

extensions/fedex/purplship/carriers/fedex/package/pickup/create_service.py
@@ -1,0 +1,114 @@
+from typing import List, Tuple
+from pyfedex.pickup_service_v20 import (
+    CreatePickupRequest,
+    TransactionDetail,
+    VersionId,
+    PickupOriginDetail,
+    ContactAndAddress,
+    AssociatedAccount,
+    AssociatedAccountNumberType,
+    Address,
+    Contact,
+    Weight,
+    WeightUnits,
+    CarrierCodeType
+)
+from purplship.core.models import PickupRequest, PickupDetails, Message
+from purplship.core.units import Package
+from purplship.core.utils import (
+    Serializable, export, create_envelope, apply_namespaceprefix, Envelope, concat_str, Element
+)
+from purplship.carriers.fedex.utils import Settings
+from purplship.carriers.fedex.error import parse_error_response
+
+
+def parse_pickup_response(response: Element, settings: Settings) -> Tuple[PickupDetails, List[Message]]:
+    pickup = (
+        _extract_pickup_details(response, settings)
+    )
+    return pickup, parse_error_response(response, settings)
+
+
+def _extract_pickup_details(response: Element, settings: Settings) -> PickupDetails:
+
+    return PickupDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+        confirmation_number=None,
+        pickup_date=None,
+        pickup_charge=None,
+        ready_time=None,
+        closing_time=None
+    )
+
+
+def pickup_request(payload: PickupRequest, settings: Settings) -> Serializable[CreatePickupRequest]:
+    packages: List[Package] = [Package(parcel) for parcel in payload.parcels]
+
+    request = CreatePickupRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="disp", Major=17, Intermediate=0, Minor=0),
+        AssociatedAccountNumber=AssociatedAccount(
+            Type=AssociatedAccountNumberType.FEDEX_EXPRESS,
+            AccountNumber=settings.account_number
+        ),
+        TrackingNumber=None,
+        OriginDetail=PickupOriginDetail(
+            UseAccountAddress=None,
+            PickupLocation=ContactAndAddress(
+                Contact=Contact(
+                    ContactId=None,
+                    PersonName=payload.address.person_name,
+                    CompanyName=payload.address.company_name,
+                    PhoneNumber=payload.address.phone_number,
+                    EMailAddress=payload.address.email
+                ),
+                Address=Address(
+                    StreetLines=concat_str(payload.address.address_line1, payload.address.address_line2),
+                    City=payload.address.city,
+                    StateOrProvinceCode=payload.address.state_code,
+                    PostalCode=payload.address.postal_code,
+                    CountryCode=payload.address.country_code,
+                    Residential=payload.address.residential,
+                )
+            ),
+            PackageLocation=payload.package_location,
+            ReadyTimestamp=f'{payload.ready_time}:00',
+            CompanyCloseTime=f'{payload.closing_time}:00',
+            PickupDateType=None,
+            LastAccessTime=None,
+            GeographicalPostalCode=None,
+            Location=None,
+            DeleteLastUsed=None,
+            SuppliesRequested=None,
+            EarlyPickup=None
+        ),
+        PickupServiceCategory=None,
+        FreightPickupDetail=None,
+        ExpressFreightDetail=None,
+        PackageCount=len(packages),
+        TotalWeight=Weight(
+            Units=WeightUnits.LB.name,
+            Value=sum((p.weight.LB or 0.0) for p in packages)
+        ),
+        CarrierCode=CarrierCodeType.FDXE.name,
+        OversizePackageCount=None,
+        Remarks=payload.instruction,
+        CommodityDescription=None,
+        CountryRelationship=None
+    )
+
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: CreatePickupRequest) -> str:
+    envelope: Envelope = create_envelope(body_content=request)
+    envelope.Body.ns_prefix_ = envelope.ns_prefix_
+    apply_namespaceprefix(envelope.Body.anytypeobjs_[0], "v17")
+
+    return export(
+        envelope,
+        namespacedef_='xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v17="http://fedex.com/ws/pickup/v17"'
+    )

lucasduarte0/karrio

extensions/canadapost/purplship/providers/canadapost/pickup/cancel_pickup.py
@@ -1,0 +1,20 @@
+from typing import Tuple, List
+from purplship.core.models import PickupCancellationRequest, Message, ConfirmationDetails
+from purplship.core.utils import Serializable, Element
+from purplship.providers.canadapost.error import parse_error_response
+from purplship.providers.canadapost.utils import Settings
+
+
+def parse_cancel_pickup_response(response: Element, settings: Settings) -> Tuple[ConfirmationDetails, List[Message]]:
+    errors = parse_error_response(response, settings)
+    cancellation = ConfirmationDetails(
+        carrier_id=settings.carrier_id,
+        carrier_name=settings.carrier_name,
+        success=True
+    ) if len(errors) == 0 else None
+
+    return cancellation, errors
+
+
+def cancel_pickup_request(payload: PickupCancellationRequest, _) -> Serializable[str]:
+    return Serializable(payload.confirmation_number)

extensions/canadapost/purplship/providers/canadapost/pickup/create_pickup.py
@@ -1,0 +1,33 @@
+from functools import partial
+from pycanadapost.pickup import pickup_availability
+from purplship.core.utils.pipeline import Job, Pipeline
+from purplship.core.utils import to_xml
+from purplship.core.models import PickupRequest
+from purplship.providers.canadapost.utils import Settings
+from purplship.providers.canadapost.pickup.request_pickup import pickup_request
+
+
+def create_pickup_request(payload: PickupRequest, settings: Settings):
+    return Pipeline(
+        get_availability=lambda *_: partial(_get_pickup_availability, payload=payload)(),
+        create_pickup=partial(_create_pickup, payload=payload, settings=settings)
+    )
+
+
+def _create_pickup(availability_response: str, payload: PickupRequest, settings: Settings):
+    availability = pickup_availability()
+    node = next(
+        iter(to_xml(availability_response).xpath(".//*[local-name() = $name]", name="pickup-availability")), None
+    )
+    if node is not None:
+        availability.build(node)
+
+    return Job(
+        id="create_pickup",
+        data=pickup_request(payload, settings) if availability.on_demand_tour else None,
+        fallback=""
+    )
+
+
+def _get_pickup_availability(payload: PickupRequest):
+    return Job(id="availability", data=payload.address.postal_code)

extensions/fedex/purplship/providers/fedex/pickup/availability_service.py
@@ -1,0 +1,61 @@
+from pyfedex.pickup_service_v20 import (
+    PickupAvailabilityRequest,
+    TransactionDetail,
+    VersionId,
+    Address,
+    CarrierCodeType,
+    PickupRequestType,
+    AssociatedAccount,
+    AssociatedAccountNumberType
+)
+from purplship.core.models import PickupRequest
+from purplship.core.utils import Serializable, export, create_envelope, apply_namespaceprefix, Envelope, concat_str
+from purplship.providers.fedex.utils import Settings
+
+
+def pickup_availability_request(payload: PickupRequest, settings: Settings) -> Serializable[PickupAvailabilityRequest]:
+    request_type = next(
+        (PickupRequestType[s] for s in PickupRequestType.__members__ if s.name == payload.service),
+        PickupRequestType.FUTURE_DAY
+    ).value
+
+    request = PickupAvailabilityRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="disp", Major=17, Intermediate=0, Minor=0),
+        PickupType=None,
+        AccountNumber=AssociatedAccount(
+            Type=AssociatedAccountNumberType.FEDEX_EXPRESS,
+            AccountNumber=settings.account_number
+        ),
+        PickupAddress=Address(
+            StreetLines=concat_str(payload.address.address_line1, payload.address.address_line2),
+            City=payload.address.city,
+            StateOrProvinceCode=payload.address.state_code,
+            PostalCode=payload.address.postal_code,
+            CountryCode=payload.address.country_code,
+            Residential=payload.address.residential,
+        ),
+        PickupRequestType=request_type,
+        DispatchDate=payload.date,
+        NumberOfBusinessDays=None,
+        PackageReadyTime=f'{payload.ready_time}:00',
+        CustomerCloseTime=f'{payload.closing_time}:00',
+        Carriers=[CarrierCodeType.FDXE],
+        ShipmentAttributes=None,
+        PackageDetails=None
+    )
+
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: PickupAvailabilityRequest) -> str:
+    envelope: Envelope = create_envelope(body_content=request)
+    envelope.Body.ns_prefix_ = envelope.ns_prefix_
+    apply_namespaceprefix(envelope.Body.anytypeobjs_[0], "v17")
+
+    return export(
+        envelope,
+        namespacedef_='xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v17="http://fedex.com/ws/pickup/v17"'
+    )

extensions/fedex/purplship/providers/fedex/pickup/cancel_service.py
@@ -1,0 +1,56 @@
+from typing import Tuple, List
+from pyfedex.pickup_service_v20 import (
+    CancelPickupRequest,
+    TransactionDetail,
+    VersionId,
+    CarrierCodeType
+)
+from purplship.core.models import PickupCancellationRequest, ConfirmationDetails, Message
+from purplship.core.utils import Serializable, export, create_envelope, apply_namespaceprefix, Envelope, Element
+from purplship.providers.fedex.error import parse_error_response
+from purplship.providers.fedex.utils import Settings
+
+
+def parse_cancel_pickup_reply(response: Element, settings: Settings) -> Tuple[ConfirmationDetails, List[Message]]:
+    errors = parse_error_response(response, settings)
+    cancellation = ConfirmationDetails(
+        carrier_id=settings.carrier_id,
+        carrier_name=settings.carrier_name,
+        success=True
+    ) if len(errors) == 0 else None
+
+    return cancellation, errors
+
+
+def cancel_pickup_request(payload: PickupCancellationRequest, settings: Settings) -> Serializable[CancelPickupRequest]:
+
+    request = CancelPickupRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="disp", Major=17, Intermediate=0, Minor=0),
+        CarrierCode=[CarrierCodeType.FDXE.value],
+        PickupConfirmationNumber=payload.confirmation_number,
+        ScheduledDate=payload.pickup_date,
+        EndDate=None,
+        Location=None,
+        Remarks=None,
+        ShippingChargesPayment=None,
+        Reason=payload.reason,
+        ContactName=payload.address.person_name,
+        PhoneNumber=payload.address.phone_number,
+        PhoneExtension=None
+    )
+
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: CancelPickupRequest) -> str:
+    envelope: Envelope = create_envelope(body_content=request)
+    envelope.Body.ns_prefix_ = envelope.ns_prefix_
+    apply_namespaceprefix(envelope.Body.anytypeobjs_[0], "v17")
+
+    return export(
+        envelope,
+        namespacedef_='xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v17="http://fedex.com/ws/pickup/v17"'
+    )

extensions/fedex/purplship/providers/fedex/pickup/create_service.py
@@ -1,0 +1,110 @@
+from typing import List, Tuple
+from pyfedex.pickup_service_v20 import (
+    CreatePickupRequest,
+    TransactionDetail,
+    VersionId,
+    PickupOriginDetail,
+    ContactAndAddress,
+    AssociatedAccount,
+    AssociatedAccountNumberType,
+    Address,
+    Contact,
+    Weight,
+    WeightUnits,
+    CarrierCodeType
+)
+from purplship.core.models import PickupRequest, PickupDetails, Message
+from purplship.core.units import Package
+from purplship.core.utils import (
+    Serializable, export, create_envelope, apply_namespaceprefix, Envelope, concat_str, Element
+)
+from purplship.providers.fedex.utils import Settings
+from purplship.providers.fedex.error import parse_error_response
+
+
+def parse_pickup_response(response: Element, settings: Settings) -> Tuple[PickupDetails, List[Message]]:
+    pickup = (
+        _extract_pickup_details(response, settings)
+    )
+    return pickup, parse_error_response(response, settings)
+
+
+def _extract_pickup_details(response: Element, settings: Settings) -> PickupDetails:
+
+    return PickupDetails(
+        carrier_id=settings.carrier_id,
+        carrier_name=settings.carrier_name,
+        confirmation_number="",
+    )
+
+
+def pickup_request(payload: PickupRequest, settings: Settings) -> Serializable[CreatePickupRequest]:
+    packages: List[Package] = [Package(parcel) for parcel in payload.parcels]
+
+    request = CreatePickupRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="disp", Major=17, Intermediate=0, Minor=0),
+        AssociatedAccountNumber=AssociatedAccount(
+            Type=AssociatedAccountNumberType.FEDEX_EXPRESS,
+            AccountNumber=settings.account_number
+        ),
+        TrackingNumber=None,
+        OriginDetail=PickupOriginDetail(
+            UseAccountAddress=None,
+            PickupLocation=ContactAndAddress(
+                Contact=Contact(
+                    ContactId=None,
+                    PersonName=payload.address.person_name,
+                    CompanyName=payload.address.company_name,
+                    PhoneNumber=payload.address.phone_number,
+                    EMailAddress=payload.address.email
+                ),
+                Address=Address(
+                    StreetLines=concat_str(payload.address.address_line1, payload.address.address_line2),
+                    City=payload.address.city,
+                    StateOrProvinceCode=payload.address.state_code,
+                    PostalCode=payload.address.postal_code,
+                    CountryCode=payload.address.country_code,
+                    Residential=payload.address.residential,
+                )
+            ),
+            PackageLocation=payload.package_location,
+            ReadyTimestamp=f'{payload.ready_time}:00',
+            CompanyCloseTime=f'{payload.closing_time}:00',
+            PickupDateType=None,
+            LastAccessTime=None,
+            GeographicalPostalCode=None,
+            Location=None,
+            DeleteLastUsed=None,
+            SuppliesRequested=None,
+            EarlyPickup=None
+        ),
+        PickupServiceCategory=None,
+        FreightPickupDetail=None,
+        ExpressFreightDetail=None,
+        PackageCount=len(packages),
+        TotalWeight=Weight(
+            Units=WeightUnits.LB.name,
+            Value=sum((p.weight.LB or 0.0) for p in packages)
+        ),
+        CarrierCode=CarrierCodeType.FDXE.name,
+        OversizePackageCount=None,
+        Remarks=payload.instruction,
+        CommodityDescription=None,
+        CountryRelationship=None
+    )
+
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: CreatePickupRequest) -> str:
+    envelope: Envelope = create_envelope(body_content=request)
+    envelope.Body.ns_prefix_ = envelope.ns_prefix_
+    apply_namespaceprefix(envelope.Body.anytypeobjs_[0], "v17")
+
+    return export(
+        envelope,
+        namespacedef_='xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v17="http://fedex.com/ws/pickup/v17"'
+    )