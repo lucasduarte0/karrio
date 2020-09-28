QwadwoNyamekye/purplship

WIP: drafting address validation request mappers

extensions/dhl/purplship/carriers/dhl/route_request.py
@@ -1,0 +1,52 @@
+from typing import Tuple, List
+from pydhl.routing_global_req_2_0 import (
+    RouteRequest,
+    RequestTypeType,
+    MetaData,
+)
+from purplship.core.utils import Serializable, export, Element, concat_str
+from purplship.core.models import AddressValidationRequest, Message, AddressValidationDetails
+from purplship.carriers.dhl.units import CountryRegion
+from purplship.carriers.dhl.utils import Settings
+from purplship.carriers.dhl.error import parse_error_response
+
+
+def parse_route_response(response: Element, settings: Settings) -> Tuple[AddressValidationDetails, List[Message]]:
+    validation_details = _extract_address_validation(response, settings)
+    return validation_details, parse_error_response(response, settings)
+
+
+def _extract_address_validation(node: Element, settings: Settings) -> AddressValidationDetails:
+
+    return AddressValidationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+    )
+
+
+def route_request(payload: AddressValidationRequest, settings: Settings) -> Serializable[RouteRequest]:
+
+    request = RouteRequest(
+        schemaVersion=2.0,
+        Request=settings.Request(
+            MetaData=MetaData(SoftwareName="3PV", SoftwareVersion=1.0)
+        ),
+        RegionCode=CountryRegion[payload.address.country_code].value,
+        RequestType=RequestTypeType.D.value,
+        Address1=concat_str(payload.address.address_line1),
+        Address2=concat_str(payload.address.address_line2),
+        Address3=None,
+        PostalCode=payload.address.postal_code,
+        City=payload.address.city,
+        Division=None,
+        CountryCode=payload.address.country_code,
+        CountryName=None,
+        OriginCountryCode=payload.address.country_code,
+    )
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: RouteRequest) -> str:
+    namespacedef_ = 'xmlns:ns1="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com routing-global-req.xsd"'
+
+    return export(request, namespacedef_=namespacedef_)

extensions/fedex/purplship/carriers/fedex/address_validation_service.py
@@ -1,0 +1,75 @@
+from typing import Tuple, List
+from pyfedex.address_validation_service_v4 import (
+    AddressValidationRequest as FedexAddressValidationRequest,
+    AddressToValidate,
+    Address,
+    Contact,
+    TransactionDetail,
+    VersionId,
+)
+from purplship.core.utils import Serializable, export, Element, concat_str
+from purplship.core.models import AddressValidationRequest, Message, AddressValidationDetails
+from purplship.core.utils.soap import create_envelope
+from purplship.carriers.fedex.utils import Settings
+from purplship.carriers.fedex.error import parse_error_response
+
+
+def parse_address_validation_response(response: Element, settings: Settings) -> Tuple[AddressValidationDetails, List[Message]]:
+    validation_details = _extract_address_validation(response, settings)
+    return validation_details, parse_error_response(response, settings)
+
+
+def _extract_address_validation(node: Element, settings: Settings) -> AddressValidationDetails:
+
+    return AddressValidationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+    )
+
+
+def address_validation_request(payload: AddressValidationRequest, settings: Settings) -> Serializable[FedexAddressValidationRequest]:
+
+    request = FedexAddressValidationRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="crs", Major=26, Intermediate=0, Minor=0),
+        InEffectAsOfTimestamp=None,
+        AddressesToValidate=[
+            AddressToValidate(
+                ClientReferenceId=None,
+                Contact=Contact(
+                    ContactId=None,
+                    PersonName=payload.address.person_name,
+                    Title=None,
+                    CompanyName=payload.address.company_name,
+                    PhoneNumber=payload.address.phone_number,
+                    PhoneExtension=None,
+                    TollFreePhoneNumber=None,
+                    PagerNumber=None,
+                    FaxNumber=None,
+                    EMailAddress=payload.address.email
+                ),
+                Address=Address(
+                    StreetLines=concat_str(payload.address.address_line1, payload.address.address_line2),
+                    City=payload.address.city,
+                    StateOrProvinceCode=payload.address.city,
+                    PostalCode=payload.address.postal_code,
+                    UrbanizationCode=None,
+                    CountryCode=payload.address.country_code,
+                    CountryName=None,
+                    Residential="" if payload.address.residential else None,
+                ),
+            )
+        ],
+    )
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: FedexAddressValidationRequest) -> str:
+    namespacedef_ = ''
+
+    return export(
+        create_envelope(body_content=request),
+        namespacedef_=namespacedef_
+    )

extensions/purolator/purplship/carriers/purolator/package/availability_service.py
@@ -1,0 +1,49 @@
+from typing import Tuple, List
+from pypurolator.service_availability_service_2_0_2 import (
+    ValidateCityPostalCodeZipRequest,
+    ArrayOfShortAddress,
+    ShortAddress,
+)
+from purplship.core.utils import Serializable, export, Element
+from purplship.core.models import AddressValidationRequest, Message, AddressValidationDetails
+from purplship.carriers.purolator.utils import Settings
+from purplship.carriers.purolator.error import parse_error_response
+
+
+def parse_validate_address_response(response: Element, settings: Settings) -> Tuple[AddressValidationDetails, List[Message]]:
+    validation_details = _extract_address_validation(response, settings)
+    return validation_details, parse_error_response(response, settings)
+
+
+def _extract_address_validation(node: Element, settings: Settings) -> AddressValidationDetails:
+
+    return AddressValidationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+    )
+
+
+def validate_address_request(payload: AddressValidationRequest, settings: Settings) -> Serializable[ValidateCityPostalCodeZipRequest]:
+
+    request = ValidateCityPostalCodeZipRequest(
+        Addresses=ArrayOfShortAddress(
+            ShortAddress=[
+                ShortAddress(
+                    City=None,
+                    Province=None,
+                    Country=None,
+                    PostalCode=None,
+                )
+            ]
+        )
+    )
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: ValidateCityPostalCodeZipRequest) -> str:
+    namespacedef_ = ''
+
+    return export(
+        request,
+        namespacedef_=namespacedef_
+    )

lucasduarte0/karrio

extensions/dhl_express/purplship/providers/dhl_express/route_request.py
@@ -1,0 +1,52 @@
+from typing import Tuple, List
+from pydhl.routing_global_req_2_0 import (
+    RouteRequest,
+    RequestTypeType,
+    MetaData,
+)
+from purplship.core.utils import Serializable, export, Element, concat_str
+from purplship.core.models import AddressValidationRequest, Message, AddressValidationDetails
+from purplship.providers.dhl.units import CountryRegion
+from purplship.providers.dhl.utils import Settings
+from purplship.providers.dhl.error import parse_error_response
+
+
+def parse_route_response(response: Element, settings: Settings) -> Tuple[AddressValidationDetails, List[Message]]:
+    validation_details = _extract_address_validation(response, settings)
+    return validation_details, parse_error_response(response, settings)
+
+
+def _extract_address_validation(node: Element, settings: Settings) -> AddressValidationDetails:
+
+    return AddressValidationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+    )
+
+
+def route_request(payload: AddressValidationRequest, settings: Settings) -> Serializable[RouteRequest]:
+
+    request = RouteRequest(
+        schemaVersion=2.0,
+        Request=settings.Request(
+            MetaData=MetaData(SoftwareName="3PV", SoftwareVersion=1.0)
+        ),
+        RegionCode=CountryRegion[payload.address.country_code].value,
+        RequestType=RequestTypeType.D.value,
+        Address1=concat_str(payload.address.address_line1),
+        Address2=concat_str(payload.address.address_line2),
+        Address3=None,
+        PostalCode=payload.address.postal_code,
+        City=payload.address.city,
+        Division=None,
+        CountryCode=payload.address.country_code,
+        CountryName=None,
+        OriginCountryCode=payload.address.country_code,
+    )
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: RouteRequest) -> str:
+    namespacedef_ = 'xmlns:ns1="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com routing-global-req.xsd"'
+
+    return export(request, namespacedef_=namespacedef_)

extensions/fedex/purplship/providers/fedex/address_validation_service.py
@@ -1,0 +1,76 @@
+from typing import Tuple, List
+from pyfedex.address_validation_service_v4 import (
+    AddressValidationRequest as FedexAddressValidationRequest,
+    AddressToValidate,
+    Address,
+    Contact,
+    TransactionDetail,
+    VersionId,
+)
+from purplship.core.utils import Serializable, export, Element, concat_str
+from purplship.core.models import AddressValidationRequest, Message, AddressValidationDetails
+from purplship.core.utils.soap import create_envelope
+from purplship.providers.fedex.utils import Settings
+from purplship.providers.fedex.error import parse_error_response
+
+
+def parse_address_validation_response(response: Element, settings: Settings) -> Tuple[AddressValidationDetails, List[Message]]:
+    validation_details = _extract_address_validation(response, settings)
+    return validation_details, parse_error_response(response, settings)
+
+
+def _extract_address_validation(node: Element, settings: Settings) -> AddressValidationDetails:
+
+    return AddressValidationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+        address=None
+    )
+
+
+def address_validation_request(payload: AddressValidationRequest, settings: Settings) -> Serializable[FedexAddressValidationRequest]:
+
+    request = FedexAddressValidationRequest(
+        WebAuthenticationDetail=settings.webAuthenticationDetail,
+        ClientDetail=settings.clientDetail,
+        TransactionDetail=TransactionDetail(CustomerTransactionId="FTC"),
+        Version=VersionId(ServiceId="crs", Major=26, Intermediate=0, Minor=0),
+        InEffectAsOfTimestamp=None,
+        AddressesToValidate=[
+            AddressToValidate(
+                ClientReferenceId=None,
+                Contact=Contact(
+                    ContactId=None,
+                    PersonName=payload.address.person_name,
+                    Title=None,
+                    CompanyName=payload.address.company_name,
+                    PhoneNumber=payload.address.phone_number,
+                    PhoneExtension=None,
+                    TollFreePhoneNumber=None,
+                    PagerNumber=None,
+                    FaxNumber=None,
+                    EMailAddress=payload.address.email
+                ),
+                Address=Address(
+                    StreetLines=concat_str(payload.address.address_line1, payload.address.address_line2),
+                    City=payload.address.city,
+                    StateOrProvinceCode=payload.address.city,
+                    PostalCode=payload.address.postal_code,
+                    UrbanizationCode=None,
+                    CountryCode=payload.address.country_code,
+                    CountryName=None,
+                    Residential="" if payload.address.residential else None,
+                ),
+            )
+        ],
+    )
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: FedexAddressValidationRequest) -> str:
+    namespacedef_ = ''
+
+    return export(
+        create_envelope(body_content=request),
+        namespacedef_=namespacedef_
+    )

extensions/purolator/purplship/providers/purolator/availability_service.py
@@ -1,0 +1,49 @@
+from typing import Tuple, List
+from pypurolator.service_availability_service_2_0_2 import (
+    ValidateCityPostalCodeZipRequest,
+    ArrayOfShortAddress,
+    ShortAddress,
+)
+from purplship.core.utils import Serializable, export, Element
+from purplship.core.models import AddressValidationRequest, Message, AddressValidationDetails
+from purplship.providers.purolator.utils import Settings
+from purplship.providers.purolator.error import parse_error_response
+
+
+def parse_validate_address_response(response: Element, settings: Settings) -> Tuple[AddressValidationDetails, List[Message]]:
+    validation_details = _extract_address_validation(response, settings)
+    return validation_details, parse_error_response(response, settings)
+
+
+def _extract_address_validation(node: Element, settings: Settings) -> AddressValidationDetails:
+
+    return AddressValidationDetails(
+        carrier=settings.carrier,
+        carrier_name=settings.carrier_name,
+    )
+
+
+def validate_address_request(payload: AddressValidationRequest, settings: Settings) -> Serializable[ValidateCityPostalCodeZipRequest]:
+
+    request = ValidateCityPostalCodeZipRequest(
+        Addresses=ArrayOfShortAddress(
+            ShortAddress=[
+                ShortAddress(
+                    City=None,
+                    Province=None,
+                    Country=None,
+                    PostalCode=None,
+                )
+            ]
+        )
+    )
+    return Serializable(request, _request_serializer)
+
+
+def _request_serializer(request: ValidateCityPostalCodeZipRequest) -> str:
+    namespacedef_ = ''
+
+    return export(
+        request,
+        namespacedef_=namespacedef_
+    )